68ab95f01be80c804b16513cec38ccf87c8a2ef0
Leliso Agegnehu <lelisoagegnehu491@gmail.com>
2021-12-06 14:20:06 +0300

    create git

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+@extends('template.footer')
+@extends('template.contact')
+@extends('template.services')
+{{-- @extends('template.scroll') --}}
+@extends('template.team')
+@extends('template.about')
+@extends('template.carusel_slider')
+@extends('template.navbar')
+
+@endsection
